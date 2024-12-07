06290f327b16bbf1fd03e506b6333390c75885c5
Ihezie Ernest <iernest61@gmail.com>
2024-12-07 17:19:34 +0100

    Updated wsgi and development_settings

--- blog_backend/wsgi.py
+++ blog_backend/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 settings_module = (
-    "api.deployment_settings"
+    "blog_backend.deployment_settings"
     if "RENDER_EXTERNAL_HOSTNAME" in os.environ
     else "blog_backend.settings"
 )
