8625ecb238bf66dd14791bd6c5a5bfe9383298bf
Ihezie Ernest <iernest61@gmail.com>
2024-12-09 16:19:16 +0100

    deleted wrong import from project url

--- blog_backend/urls.py
+++ blog_backend/urls.py
@@ -20,7 +20,6 @@ from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-from rest_framework.schemas import get_schema_view
 
 schema_view = get_schema_view(
     openapi.Info(
