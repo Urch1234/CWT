17088af1e395a11a72bcbf65fbd540dea6651adb
Ihezie Ernest <iernest61@gmail.com>
2024-12-12 09:31:38 +0100

    Temporal AllowAny in settings

--- blog_backend/settings.py
+++ blog_backend/settings.py
@@ -64,7 +64,7 @@ SITE_ID = 1  # Required by django-allauth
 # Added
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.AllowAny",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
